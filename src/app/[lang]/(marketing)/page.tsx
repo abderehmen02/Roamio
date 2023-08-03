73ddc941587438a2bda900bf17b1e6c1ca9f600a
abderrahmane <ilahalaha0987@gmail.com>
2023-08-03 16:42:48 +0100

    translation is working

--- src/app/[lang]/(marketing)/page.tsx
+++ src/app/[lang]/(marketing)/page.tsx
@@ -1,7 +1,10 @@
 import { UnlogedHeader } from "@/components/layout/unlogedHeader"
-
-export default function Home (){
+import {useTranslation} from '../../i18n'
+import { LangParam } from "@/types/routing"
+export default async  function Home ({ params: { lang }} : LangParam){
+    const {t} = await useTranslation(lang)
     return <div className="flex w-full flex-col bg-white" >
 <UnlogedHeader/>
+<h1>{t("title")}</h1>
     </div>
 }
---
+++ src/types/routing.ts
@@ -0,0 +1,5 @@
+export interface LangParam {
+    params : {
+        lang :string
+    }
+}
