cd7299d94d40c69a96dcfe8acc7fa865ef1814f3
abderrahmane <ilahalaha0987@gmail.com>
2023-08-08 16:04:10 +0100

    integrating sign up form

--- src/app/[lang]/(marketing)/(auth)/signUp/_components/signUpForm.tsx
+++ src/app/[lang]/(marketing)/(auth)/signUp/_components/signUpForm.tsx
@@ -1,18 +1,22 @@
 "use client"
 
-import { useTranslation } from "@/app/i18n"
+import { useTranslation } from "@/app/i18n/client"
 import { PrimaryInput } from "@/ui/input"
 import { Title } from "@/ui/title"
 import { P } from "@/ui/typography"
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
-export const SignUpForm = async ()=>{
-    const {t} = await useTranslation()
-    return <div className="bg-white flex flex-col" >
+export const SignUpForm = ()=>{
+    const {t} = useTranslation()
+    return <div className="bg-white px-10 py-5 flex flex-col" >
 <Title title={t("signUp.title")}  descreption={t("signUp.titleDescreption")} />
 <PrimaryInput  label={t("signUp.firstName")}  />
 <PrimaryInput label={t("signUp.lastName")}  />
 <PrimaryInput label={t("signUp.userName")}  action={<P>{t("signUp.generateUsername")}</P>}  helperText={t("signUp.helperUserName")} />
 <PrimaryInput type="email"   label={t("signUp.email")}  />
-<PrimaryInput  type="date" label="birth date" />
-    </div>
+<LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker />
+    </LocalizationProvider>    </div>
 }
--- src/app/[lang]/(marketing)/(auth)/signUp/page.tsx
+++ src/app/[lang]/(marketing)/(auth)/signUp/page.tsx
@@ -1,7 +1,9 @@
+import { SignUpForm } from "./_components/signUpForm";
+
 export  default  function SignUpPage(){
     return <div>
         <div>
-            
+            <SignUpForm/>
         </div>
     </div>
 }
