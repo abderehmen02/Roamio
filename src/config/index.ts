49ee8c85c5b020d7cec9412394b68e7b6e2d4fd2
abderrahmane <ilahalaha0987@gmail.com>
2023-08-20 09:59:41 +0100

    adding the app config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,15 @@
+enum modes {
+    DEVELOPMENT ,
+    PRODUCTION
+}
+
+
+const mode= process.env.NODE_ENV   == "development"  ? modes.DEVELOPMENT : modes.PRODUCTION  
+const developmentUrl = "http://localhost:3000/"
+const productionUrl = "http://roamio.fr/"
 export const appConfig = {
-    dateFormate : 'MM/dd/yyyy'
+    dateFormate : 'MM/dd/yyyy' ,
+    name : 'Roamio' ,
+    mode  ,
+    url : mode  === modes.DEVELOPMENT ? developmentUrl : productionUrl
 }
