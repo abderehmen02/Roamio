27c8403de3d4b33c6ccf8f0f51845c49df5cff36
abderrahmane <ilahalaha0987@gmail.com>
2023-08-22 16:53:09 +0100

    getting the code in the api handler

--- src/app/api/(auth)/googleAuth/route.ts
+++ src/app/api/(auth)/googleAuth/route.ts
@@ -1,3 +1,7 @@
-export const GET = (req: Request)=>{
 
+export const GET = (req: Request)=>{
+    console.log("getting google auth request")
+    const {searchParams} = new URL(req.url)
+    const code = searchParams.get("code")
+    console.log("code" ,code)
 }
