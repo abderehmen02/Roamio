701d10a8b480b7264b669f80d2352343b67faaed
abderrahmane <ilahalaha0987@gmail.com>
2023-08-22 16:42:28 +0100

    adding sign in with google href link

---
+++ src/utils/auth/googleAuth/getGoogleAuthUrl.ts
@@ -0,0 +1,26 @@
+export const getGoogleAuthUrl =   (): string=> {
+
+
+if(!process.env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID){
+    const error = "can not get the client id from the envirement variables" 
+    console.log(error)
+    throw new Error(error)
+}
+
+    const baseUrl = "https://accounts.google.com/o/oauth2/v2/auth"
+
+    const options = {
+        redirect_uri : 'http://localhost:3000/api/googleAuth' ,
+        client_id: process.env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID ,
+        access_type: "offline"  ,
+        response_type : "code"  ,
+        prompt : 'consent' ,
+        scope: ["https://www.googleapis.com/auth/userinfo.profile" , "https://www.googleapis.com/auth/userinfo.email"].join(" ")
+    }
+    
+    
+    const qs = new URLSearchParams(options)
+    
+
+return `${baseUrl}?${qs.toString()}`
+}
