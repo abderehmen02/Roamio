c374ea5017cdfbaf8cc239a90d5aa5f2d01ae722
abderrahmane <ilahalaha0987@gmail.com>
2023-08-31 20:37:52 +0100

    maping the cities

--- src/app/[lang]/(logged)/dashboard/_components/cityCard.tsx
+++ src/app/[lang]/(logged)/dashboard/_components/cityCard.tsx
@@ -1,12 +1,22 @@
+"use client"
+
+import { useTranslation } from "@/app/i18n/client"
 import { CityDb } from "@/db/models/city"
 import { useCityWikipediaData } from "@/hooks/cityWikipediaData"
-import { useQuery } from "@tanstack/react-query"
-import Image from "next/image"
+import { Title } from "@/ui/title"
+import {  P } from "@/ui/typography"
 
-export const CityCard : React.FC<CityDb> = (city)=>{
-    const data  = useCityWikipediaData(city.name)
-    console.log("data"  , data)
-    return <div>
-        city card
+export const CityCard : React.FC<CityDb> =  (city)=>{
+    const cityWikipediaData   =  useCityWikipediaData(city.name)
+    const {t} = useTranslation()
+    if(cityWikipediaData.error || !cityWikipediaData.infoAvailble || cityWikipediaData.loading ) return null
+     const {descreption , image , lat , lon , subtitle} = cityWikipediaData
+    return <div className="flex items-center " >
+    <img src={image}  />
+    <div className="flex flex-col" >
+     <Title title={city.name}  descreption={subtitle}  />
+     <P>{descreption}</P>
+     <div className="flex" ><P>{city.continent} </P><P>{t("languages")} : {city.languages.map(lan=><span>{lan}, </span>)}</P><P>{t("cityCard.latitude")} : {lat} </P> <P>{t("cityCard.longitude")} : {lon} </P> </div>
+    </div>
     </div>
 }
