3f9a2400b75fe0cc03e4f72e0330f5b0393e350a
abderrahmane <ilahalaha0987@gmail.com>
2023-09-04 14:54:35 +0100

    updating the get cities route

--- src/app/api/(cities)/getCities/route.ts
+++ src/app/api/(cities)/getCities/route.ts
@@ -1,41 +1,26 @@
-import { Continents, cityModal } from "@/db/models/city"
-import { Prices } from "@/types/prefrences"
+import { connectDbPromise } from "@/db/connect"
+import { CityDb, historicalModal, modelsMap } from "@/db/models/city"
+import { Categories, Category, Prices } from "@/types/prefrences"
 import { apiResponse } from "@/utils/api/nextResponse"
 import { asyncWrapperApi } from "@/utils/asyncWrapper"
 
 
+export const  getCategories : (categories: Category[]) => Promise<CityDb[]> = async (categories )  =>{
+let cities : CityDb[] = []
 
 
+for(let category of categories){
+      if(category === Categories.Historical || category === Categories.Romantic ){
+      let modelCities : CityDb[] =  await      modelsMap[Categories.Historical].find({}) 
+      modelCities.forEach(city=>cities.push(city)  )      
+      }
+}
+return cities
+}
 export const GET = asyncWrapperApi(async (req )=>{
       const {searchParams} = new URL(req.url)
-      const name = searchParams.get("name") 
-      const countries = searchParams.get("countries")
-      const continents = searchParams.get("continents")
-      const population = searchParams.get("population") 
-      const categories = searchParams.get("categories")
-      const prices = searchParams.get("prices")
-      const weathers = searchParams.get("weathers") 
-      const yearTimes = searchParams.get("yearTimes")
-      const languages =    searchParams.get("languages")
-   
-const queryDb : any = {}  ; 
-console.log("querydb" , queryDb)
-if(name ) queryDb.name = name 
-if(countries) queryDb.country = {$in: JSON.parse(countries)} 
-if(continents) queryDb.continent = {$in: JSON.parse(continents)} 
-if(population) queryDb.population = population 
-if(categories) queryDb.categories =  {$in: JSON.parse(categories)}
-if(prices) queryDb.price = {$in: JSON.parse(prices)}
-if(weathers) queryDb.weathers = { $in :   JSON.parse(weathers)}
-if(yearTimes) queryDb.yearTimes = { $in : JSON.parse(yearTimes)}
-if(languages) queryDb.languages = { $in : JSON.parse( languages)}
-
-if(!Object.keys(queryDb).length) {
-      queryDb.continent = {$in: [Continents.EUROPE  ]} 
-      queryDb.price  = {$in : [Prices.costly , Prices.expensive , Prices.medium  ] } 
-}
-console.log("query db"   , queryDb)
-       const cities = await cityModal().find(queryDb)
+      const categories  : Category[] | null =   JSON.parse(searchParams.get("category") || '[]' )
+      let cities : CityDb[] = []
 
       return  apiResponse(200 , cities)
 })
--- src/db/models/city.ts
+++ src/db/models/city.ts
@@ -1,47 +1,34 @@
-import { Category, Language, Price, Weather, YearTime } from "@/types/prefrences";
-import { LanguageDetectorAsyncModule } from "i18next";
+import { Category , Categories , Language, Price, Weather, YearTime } from "@/types/prefrences";
 import mongoose , {model , mongo, Schema} from "mongoose";
 
-export const Continents = {
-  EUROPE : "Europe" ,
-  AFRICA : "Africa" ,
-  ASIA : "ASIA" ,
-  NORTH_AMERICA : "NORTH_AMERICA" ,
-  SOUTH_AMERICA : "SOURTH_AMERICA" ,
-  ANTARCTICA :  "ANTARCTICA"  ,
-  AUSTRALIA : "AUSTRALIA"
-} as const 
-export const continentsArray = Object.values(Continents)
-export type Continent = typeof continentsArray[number]
+
+
 
 export interface CityDb {
     name: string,
-    country: string,
-    continent: Continent ,
-    population: number ,
-    categories: Category[],
-    price: Price[],
-    weathers: Weather[],
-    yearTimes: YearTime[],
-    languages: Language[] ,
-    _id: string
+    landmarks : string[]
   }
+
 
 
 
 const CitySchema = new mongoose.Schema<CityDb>({
     name: { type: String, required: true },
-    country: { type: String, required: false },
-    continent : {type : String , required : true } ,
-    population: { type: Number, required: true },
-    categories : {type : [String] , required : true} ,
-    price  : {type : [String] , required : true} ,
-    weathers : {type : [String] , required : true} ,
-    yearTimes : {type : [String] , required  : true}  ,
-    languages : {type : [String] , required : true}
+    landmarks : {type : [String] , required : false }
   });
   
-export const cityModal  = ()=>{
-    return mongoose.models?.city || model<CityDb>("city" , CitySchema)
+export const historicalModal  = ()=>{
+    return mongoose.models?.[Categories.Historical] || model<CityDb>(Categories.Historical, CitySchema)
 }
 
+
+export const romanticModal  = ()=>{
+  return mongoose.models?.[Categories.Romantic]  || model<CityDb>(Categories.Romantic , CitySchema)
+}
+
+
+
+export const modelsMap = {
+  [Categories.Historical] : historicalModal() ,
+  [Categories.Romantic] : romanticModal()
+}
