cb198258dc1d3fd708ae7d9ba75209d6d73b0ab6
abderrahmane <ilahalaha0987@gmail.com>
2023-09-09 09:54:20 +0100

    changing the api error message

--- src/app/api/(cities)/(likes)/addDislike/route.ts
+++ src/app/api/(cities)/(likes)/addDislike/route.ts
@@ -1,7 +1,7 @@
-import { CityDb, cityModal} from "@/db/models/city"
+import {  cityModal} from "@/db/models/city"
 import { errorMessage } from "@/utils/api/error"
 import { apiResponse } from "@/utils/api/nextResponse"
-import { asyncWrapperApi, asyncWrapperAuthorisedApi } from "@/utils/asyncWrapper"
+import { asyncWrapperAuthorisedApi } from "@/utils/asyncWrapper"
 import { likesRequestValidator } from "@/utils/validators/cities"
 import StatusCodes from 'http-status-codes'
 
@@ -10,12 +10,12 @@ import StatusCodes from 'http-status-codes'
 export const PATCH = asyncWrapperAuthorisedApi(async (req , userInfo)=>{ 
     const body = await  req.json()
     const parsedBody   = likesRequestValidator.safeParse(body)
-    if(!parsedBody.success)       return  apiResponse(StatusCodes.BAD_REQUEST , errorMessage("the request must include the city name of the post "))
+    if(!parsedBody.success)       return  apiResponse(StatusCodes.BAD_REQUEST , errorMessage("the request must the city name of the post "))
     const  { city   } = parsedBody.data
     const cityInfo = await cityModal().findOne({name : city})
     if(!cityInfo) return  apiResponse(StatusCodes.BAD_REQUEST  , errorMessage("no city found in the database"))
-    if(!cityInfo.likes.includes(userInfo.userId)) return apiResponse(StatusCodes.BAD_REQUEST , errorMessage("City is not liked yet!"))
-    const newCityInfo = await cityModal().findOneAndUpdate({name :city } , { likes :  cityInfo.likes.filter((item : string )=>item !== userInfo.userId)  } , {new:  true} )
+    if(cityInfo.likes.includes(userInfo.userId)) return apiResponse(StatusCodes.BAD_REQUEST , errorMessage("you already liked this city"))
+    const newCityInfo = await cityModal().findOneAndUpdate({name :city } , { likes :  [ ...cityInfo.likes , userInfo.userId ] } , {new:  true} )
     
     return apiResponse(StatusCodes.CREATED , newCityInfo)
 })
--- src/app/api/(cities)/(likes)/addLike/route.ts
+++ src/app/api/(cities)/(likes)/addLike/route.ts
@@ -1,9 +1,7 @@
-import { connectDbPromise } from "@/db/connect"
-import { CityDb, cityModal} from "@/db/models/city"
-import { Categories, Category, Prices } from "@/types/prefrences"
+import {  cityModal} from "@/db/models/city"
 import { errorMessage } from "@/utils/api/error"
 import { apiResponse } from "@/utils/api/nextResponse"
-import { asyncWrapperApi, asyncWrapperAuthorisedApi } from "@/utils/asyncWrapper"
+import { asyncWrapperAuthorisedApi } from "@/utils/asyncWrapper"
 import { likesRequestValidator } from "@/utils/validators/cities"
 import StatusCodes from 'http-status-codes'
 
@@ -12,7 +10,7 @@ import StatusCodes from 'http-status-codes'
 export const PATCH = asyncWrapperAuthorisedApi(async (req , userInfo)=>{ 
     const body = await  req.json()
     const parsedBody   = likesRequestValidator.safeParse(body)
-    if(!parsedBody.success)       return  apiResponse(StatusCodes.BAD_REQUEST , errorMessage("the request must include the userName of the user "))
+    if(!parsedBody.success)       return  apiResponse(StatusCodes.BAD_REQUEST , errorMessage("the request must include the city name of the post "))
     const  { city   } = parsedBody.data
     const cityInfo = await cityModal().findOne({name : city})
     if(!cityInfo) return  apiResponse(StatusCodes.BAD_REQUEST  , errorMessage("no city found in the database"))
