528adb9d42c2c58b29e9a535a53669997e711ee8
abderrahmane <ilahalaha0987@gmail.com>
2023-09-09 10:44:51 +0100

    integrating the dislike api

---
+++ src/app/api/(cities)/(likes)/cancelDislike/route.ts
@@ -0,0 +1,21 @@
+import {  cityModal} from "@/db/models/city"
+import { errorMessage } from "@/utils/api/error"
+import { apiResponse } from "@/utils/api/nextResponse"
+import {  asyncWrapperAuthorisedApi } from "@/utils/asyncWrapper"
+import { likesRequestValidator } from "@/utils/validators/cities"
+import StatusCodes from 'http-status-codes'
+
+
+
+export const PATCH = asyncWrapperAuthorisedApi(async (req , userInfo)=>{ 
+    const body = await  req.json()
+    const parsedBody   = likesRequestValidator.safeParse(body)
+    if(!parsedBody.success)       return  apiResponse(StatusCodes.BAD_REQUEST , errorMessage("the request must include the city name of the post "))
+    const  { city   } = parsedBody.data
+    const cityInfo = await cityModal().findOne({name : city})
+    if(!cityInfo) return  apiResponse(StatusCodes.BAD_REQUEST  , errorMessage("no city found in the database"))
+    if(!cityInfo.dislikes.includes(userInfo.userId)) return apiResponse(StatusCodes.BAD_REQUEST , errorMessage("City is not dislike yet!"))
+    const newCityInfo = await cityModal().findOneAndUpdate({name :city } , { dislikes :  cityInfo.dislikes.filter((item : string )=>item !== userInfo.userId)  } , {new:  true} )
+    
+    return apiResponse(StatusCodes.CREATED , newCityInfo)
+})
