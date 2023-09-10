73cc10dada86deecf24408fbc98c7413ca8e7de7
abderrahmane <ilahalaha0987@gmail.com>
2023-09-10 10:20:31 +0100

    styling the comment modal

--- src/app/api/(cities)/(reviews)/addCityReview/route.ts
+++ src/app/api/(cities)/(reviews)/addCityReview/route.ts
@@ -1,16 +1,19 @@
+import { cityModal } from "@/db/models/city";
 import { userModel, userModelsMap } from "@/db/models/user";
 import { errorMessage } from "@/utils/api/error";
 import { apiResponse } from "@/utils/api/nextResponse";
 import { asyncWrapperAuthorisedApi } from "@/utils/asyncWrapper";
 import { cityReviewRequestValidator } from "@/utils/validators/cities";
 import { StatusCodes } from "http-status-codes";
-
+import {experimental_useOptimistic} from 'react'
 export const POST =  asyncWrapperAuthorisedApi(async (req, userInfo)=>{
 const body = await req.json()
 const parsedData  = cityReviewRequestValidator.safeParse(body) 
-if(!parsedData.success) return apiResponse(StatusCodes.BAD_REQUEST , errorMessage("the request should include a review and it should be more than 1 character "))
+if(!parsedData.success) return apiResponse(StatusCodes.BAD_REQUEST , errorMessage("the request should include the name of the city and a review and it should be more than 1 character "))
 const userInfoModel = userInfo.authService ? userModelsMap[userInfo.authService] : userModel()
 const dbUserInfo  = await  userInfoModel.findById(userInfo.userId)
-console.log("db user ifno" , dbUserInfo )
+const userName = dbUserInfo.userName || dbUserInfo.given_name
+const image = dbUserInfo.image || "/unknownPerson"
+const newCity = await cityModal().findOneAndUpdate({name : parsedData.data.city  } ,{} )
 return apiResponse(StatusCodes.CREATED , true )
 })
--- src/modals/city/commentModal.tsx
+++ src/modals/city/commentModal.tsx
@@ -2,10 +2,11 @@ import { useTranslation } from "@/app/i18n/client";
 import { CityDb } from "@/db/models/city"
 import { Title } from "@/ui/title";
 import { H3 } from "@/ui/typography";
-import { Box, IconButton, InputBase, Modal, Paper, TextField, Typography } from "@mui/material"
+import { Box, IconButton, Input, InputBase, Modal, Paper, TextField, Typography } from "@mui/material"
 import React, { useState } from "react"
 import SendIcon from '@mui/icons-material/Send';
 import { PrimaryInput } from "@/ui/input";
+import { ButtonsSizes, PrimaryBtn } from "@/ui/buttons";
 
 const style = {
     position: 'absolute' as 'absolute',
@@ -13,6 +14,10 @@ const style = {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: 400,
+    minHeight: '60vh',
+    display :'flex' ,
+    flexDirection : 'column' ,
+    justifyContent : 'space-between' ,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
@@ -25,15 +30,22 @@ const style = {
 
 export const CommentModal : React.FC<{open : boolean , setOpen : React.Dispatch<React.SetStateAction<boolean>> , city : CityDb }> =  ({open , setOpen , city} )=>{
 const {t}  = useTranslation()
+
+
+const titleDescreption = city.reviews.length ? "See what people are saying about " + city.name + " city" :  "What do you think about" + city.name + " city"
 return    <Modal
 open={open}
+onClose={()=>setOpen(false)}
 aria-labelledby="parent-modal-title"
 aria-describedby="parent-modal-description"
 >
 <Box sx={{ ...style , width: "fit-content" }}>
-<Title title={t("Reviews")}  descreption={"See what people are saying about " + city.name + "city"}  />
-<div className="flex items-center justify-center gap-1" >
- <PrimaryInput  /> <SendIcon/>
+<Title title={t("Reviews")}  descreption={titleDescreption}  />
+<div className="flex items-center h-20 justify-center gap-1" >
+<div style={{borderWidth : 1}} className="bg-white py-2   w-full flex items-center border-black rounded-md px-2" >
+  <input className="w-full" type="text"  placeholder="Write your review" style={{outline : 'none'}} />
+  <SendIcon/>
+</div>
 </div>
 </Box>
 </Modal> 
