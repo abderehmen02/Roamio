40d4733100d7236d6e8bcbb1426d516595923fdc
abderrahmane <ilahalaha0987@gmail.com>
2023-09-12 10:06:42 +0100

    updating the prefrence ui

--- src/app/[lang]/(logged)/dashboard/_components/prefrences.tsx
+++ src/app/[lang]/(logged)/dashboard/_components/prefrences.tsx
@@ -15,6 +15,7 @@ import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
 import { CitiesActionTypes } from "@/types/state/cities"
 import { generateQueryCitiesSearchParam, isPrefrenceIncluded } from "@/utils/queryCities"
+import { cn } from "@/lib/tailwind"
 
 const rowsFields : PrefrencesArray = [{  option : PrefrencesOptions.CATEGORIES , prefrence :  Categories }  , {   option: PrefrencesOptions.PRICES , prefrence: Prices } , { option : PrefrencesOptions.YEAR_TIMES , prefrence :  YearTimes } ,  {  option :  PrefrencesOptions.MEALS , prefrence : Meals }  , {option : PrefrencesOptions.WEATHERS , prefrence : Weathers} , {option : PrefrencesOptions.LANGUAGES , prefrence : Languages}  ]
 
@@ -64,7 +65,7 @@ const toglePrefrence = (prefrence : Prefrence ): void=>{
     return <div className="flex-col py-3">
 {
 
-    items.slice(0 , LastItem).map((item=> <div className="flex  items-center gap-1" > <P className="  text-sm capitalize"  onClick={()=>toglePrefrence(item)} >{item}  </P> { isPrefrenceIncluded(queryCities , option , item ) && <i className="bi bi-record-circle text-xs"></i> } </div>))
+    items.slice(0 , LastItem).map((item=> <div   onClick={()=>toglePrefrence(item)} className={ cn( " cursor-pointer  capitalize flex items-center gap-1"  , {"text-black " :  isPrefrenceIncluded(queryCities , option , item )} )} > <P  >{item}  </P> { isPrefrenceIncluded(queryCities , option , item ) && <i className="bi bi-record-circle text-sm"></i> } </div>))
 }    
 <P className="w-full capitalize text-sm" onClick={()=>{items?.length> LastItem  && setLastItem(val=>val+ 3) , console.log("clicked") }} >{t("seeMore")}<i className="bi bi-arrow-down"></i></P>
 <P className="w-full capitalize text-sm" onClick={()=>{ items?.length> 2  &&  setLastItem(val=>val  -  3) , console.log("clicked") }} >{t("seeLess")}<i className="bi bi-arrow-up"></i></P>
@@ -73,7 +74,7 @@ const toglePrefrence = (prefrence : Prefrence ): void=>{
 
 
 export const PrefrencesRow : React.FC = ()=>{
-    return <DashboardSection className="bg-secondary text-primaryDark border-none shadow-lg h-fit" >     
+    return <DashboardSection className="bg-secondary text-primary border-none shadow-lg h-fit" >     
 {
       rowsFields.map(prefrence=><PrefrenceField prefrence={prefrence.prefrence} option={prefrence.option} />)
 }     
