4fbc07f27770b06d8bff2c04fce779ca58d36370
abderrahmane <ilahalaha0987@gmail.com>
2023-09-12 11:57:02 +0100

    updating the get cities mongodb request

--- src/app/api/(cities)/getCities/route.ts
+++ src/app/api/(cities)/getCities/route.ts
@@ -1,12 +1,17 @@
 import { connectDbPromise } from "@/db/connect"
 import { CityDb, cityModal } from "@/db/models/city"
-import { Categories, Category, Prices } from "@/types/prefrences"
+import { Categories, Category, Price, Prices } from "@/types/prefrences"
 import { apiResponse } from "@/utils/api/nextResponse"
 import { asyncWrapperApi } from "@/utils/asyncWrapper"
+import { QueryObjParams } from "@/utils/queryCities"
 
 
-export const  getCities : (categories: Category[]) => Promise<CityDb[]> = async (categories )  =>{
- let cities : CityDb[] = await cityModal().find({categories: { $in: [categories] }})
+export const  getCities : (categories: Category[] , price:Price[] ) => Promise<CityDb[]> = async (categories )  =>{
+      console.log("categories" , categories)
+ let cities : CityDb[] = await cityModal().find({
+      $or: categories.map(category => ({ categories: category }))
+    })
+ console.log("cities" , cities)
  return cities
 }
 
@@ -15,9 +20,9 @@ export const  getCities : (categories: Category[]) => Promise<CityDb[]> = async 
 export const GET = asyncWrapperApi(async (req )=>{
       console.log("get cities request")
       const {searchParams} = new URL(req.url)
-      const categories  : Category[]  =   JSON.parse(searchParams.get("categories") || '[]' )
-      console.log("caterogies" , categories)
-      let cities : CityDb[] = await  getCities(categories)
+      const categories  : Category[]  =   JSON.parse(searchParams.get(QueryObjParams.categories) || '[]' )
+      const price : Price[]  = JSON.parse(searchParams.get(QueryObjParams.price) || '[]' )
+      let cities : CityDb[] = await  getCities(categories , price  )
       return  apiResponse(200 , cities)
 })
 
