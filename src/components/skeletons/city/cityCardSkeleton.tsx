814f91c973be80b937f0d3bdd3e95f82dce67cf8
abderrahmane <ilahalaha0987@gmail.com>
2023-09-14 11:09:14 +0100

    integrating the city card skeleton

--- src/components/skeletons/city/cityCardSkeleton.tsx
+++ src/components/skeletons/city/cityCardSkeleton.tsx
@@ -1,18 +1,19 @@
+import { Title } from "@/ui/title"
 import { Skeleton } from "@mui/material"
+import { TitleSkeleton } from "./ui/title"
 
 export const CityCardSkeleton : React.FC = ()=>{
     return <div className="flex flex-col shadow-md  bg-white rounded-xl  w-full border-stone-600" >
     <div  className=" flex" >
     <Skeleton variant="rectangular"  width={300} height="100%" />
     <div className="flex px-6 w-full py-1 justify-around flex-col " >
-     <Skeleton     />
-      { descreption &&  <P className="text-sm" >{ seeAllDescreption ? descreption  :  descreption?.slice(0 ,extractedIndex ) }{ extractedIndex < descreption.length &&  (  seeAllDescreption ?  <span style={{cursor: 'pointer'}} className="capitalize " onClick={()=>setSeeAllDescreption(false)} > {t("seeLess")}</span>  :     <span style={{cursor: 'pointer'}} className="capitalize " onClick={()=>setSeeAllDescreption(true)} >...{t("seeMore")}</span> )}</P> }
-<div className={cn("flex  justify-between gap-6 py-1" , {"py-3" : seeAllDescreption })} >{ loginInfo.token?.length  &&  <div className="flex items-center justify-center gap-5 h-4  " >  
-{ isUserInfo(userInfo) && <div className="flex items-center justify-center gap-1" >{  ( city.likes.includes(userInfo._id) ? <div  onClick={ !loadingLike ? unlikeCity : undefined  } className={cn("cursor-pointer" , {"opacity-40"  : loadingLike} )} ><FavoriteIcon/></div> : <FavoriteBorderIcon className={cn("cursor-pointer" , {"opacity-40" :loadingLike }) } onClick={ !loadingLike ?  likeCity : undefined }   /> ) } {city.likes.length} </div>  }
-{ isUserInfo(userInfo) && <div className={ cn( "flex items-center justify-center gap-1"  , {"opacity-40" : loadingDislke} )} >{  ( city.dislikes.includes(userInfo._id) ?  <div onClick={ !loadingDislke ? cancelDislike : undefined }  className="cursor-pointer" > <ThumbDownIcon   /></div> : <ThumbDownOffAltIcon className={cn("cursor-pointer" ,{"opacity-40" : loadingDislke})}  onClick={ !loadingDislke ?  dislikeCity : undefined  }   /> ) } {city.dislikes.length} </div>  }
-<div onClick={()=>setOpenCommentModal(true)} ><CommentIcon style={{cursor : "pointer"}} /></div> </div>}<div className="flex gap-6 justiyf-center" ><PrimaryBtn size={ButtonsSizes.small} className="py-0 " onClick={()=>setViewLandMarks((val)=>!val)} > {viewLandMarks ? <i className="bi m-0 text-sm bi-chevron-up"></i> : <i className= "bi m-0  bi-chevron-down " ></i> } {t("Explore Landmarks")}  </PrimaryBtn><SecondaryBtn className="py-0" size={ButtonsSizes.small} >{t("Explore City")}</SecondaryBtn></div></div>
+     <TitleSkeleton  title={true} titleClassName="text-2xl" className="flex-row   items-center justify-start gap-7"  descreptionClassName="font-bold text-secondaryDark" descreption={true}  />
+     <Skeleton variant="rectangular" width='80%' height='160px' />
+<div className="flex  justify-between gap-6 py-1" ><div className="flex items-center justify-center gap-5 h-4  " >  
+ <div className="flex items-center justify-center gap-1" > <Skeleton width={50} height={50} />  </div>  
+ <div className="flex items-center justify-center gap-1"  >   <Skeleton width={50} height={50}   />       </div>  
+<div  ><Skeleton width={50} height={50} /></div> </div><div className="flex gap-6 justiyf-center" >  <Skeleton width={200} height={30} /><Skeleton width={200} height={30} /></div></div>
     </div>
     </div>
-    {  viewLandMarks && cityWikipediaData.lat && cityWikipediaData.lon &&  <Landmarks city={city} cityLat={cityWikipediaData.lat} cityLon={cityWikipediaData.lon} /> }
    </div>
 }
--- src/components/skeletons/city/ui/title.tsx
+++ src/components/skeletons/city/ui/title.tsx
@@ -2,7 +2,7 @@ import { CSSProperties, HTMLAttributes } from "react";
 import { cn } from "@/lib/tailwind";
 import { Skeleton } from "@mui/material";
 
-type  TitleProps   = {
+type  TitleSkeletonProps   = {
     title : boolean , 
     descreption? : boolean ,
     titleStyle? : CSSProperties ,
@@ -10,11 +10,10 @@ type  TitleProps   = {
     className? : string, 
     descreptionStyle? : CSSProperties ,
     descreptionClassName? :string ,
-} & HTMLAttributes<HTMLDivElement>
+} 
 
 
-
-export const TitleSkeleton : React.FC<TitleProps> = ({title , descreption   , className , titleStyle , descreptionClassName , titleClassName  , descreptionStyle  })=>{
+export const TitleSkeleton : React.FC<TitleSkeletonProps> = ({title , descreption   , className , titleStyle , descreptionClassName , titleClassName  , descreptionStyle  })=>{
 return <div  className={cn(  "w-full items-center flex flex-col" , className )}   >
 { title && <Skeleton style={titleStyle} className={cn("text-primary text-center font-bold"  , titleClassName ) }  ></Skeleton>   }
 { descreption &&  <Skeleton className={cn("text-center"  , descreptionClassName ) } style={descreptionStyle}  >{descreption}</Skeleton> }
