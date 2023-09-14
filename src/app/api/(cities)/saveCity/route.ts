df9ddc80f8a99b148f331cbbc5846b06d4ac58d9
abderrahmane <ilahalaha0987@gmail.com>
2023-09-14 16:43:44 +0100

    updating the save city route

--- src/app/api/(cities)/saveCity/route.ts
+++ src/app/api/(cities)/saveCity/route.ts
@@ -2,10 +2,15 @@ import { userModelsMap } from "@/db/models/user";
 import { errorMessage } from "@/utils/api/error";
 import { apiResponse } from "@/utils/api/nextResponse";
 import { asyncWrapperAuthorisedApi } from "@/utils/asyncWrapper";
+import { saveCityValidator } from "@/utils/validators/cities";
 import { StatusCodes } from "http-status-codes";
 
 export const PATCH = asyncWrapperAuthorisedApi( async  (req , user)=>{    
-    const newUserDb = await      userModelsMap[user.authService  || "NATIVE_USER"].findByIdAndUpdate(user.userId ,  { $push: { savedCities : user.userId } }, 
+
+    const body = await  req.json()
+    const parsedBody = saveCityValidator.safeParse(body)
+    if(!parsedBody.success) return apiResponse(StatusCodes.BAD_REQUEST , errorMessage("can not get the city from the body "))
+    const newUserDb = await      userModelsMap[user.authService  || "NATIVE_USER"].findByIdAndUpdate(user.userId ,  { $push: { savedCities : parsedBody.data.city } }, 
     { new: true, upsert: true })
     if(newUserDb) return apiResponse(StatusCodes.CREATED , newUserDb)
     return apiResponse(StatusCodes.INTERNAL_SERVER_ERROR , errorMessage("can not update the user on the database"))
