5f0384106d810584904ec7996103deb85550022b
abderrahmane <ilahalaha0987@gmail.com>
2023-09-15 21:30:25 +0100

    integrating the save cities title

--- src/app/[lang]/(logged)/savedCities/page.tsx
+++ src/app/[lang]/(logged)/savedCities/page.tsx
@@ -1,8 +1,9 @@
+import { LoggedPage } from "@/ui/containers";
 import { Title } from "@/ui/title";
 
 
 export default function SavedCities (){
-    return <div>
+    return <LoggedPage>
         <Title title="Your Cities" descreption="Your Collection of Treasured Destinations." />
-    </div>
+    </LoggedPage>
 }
