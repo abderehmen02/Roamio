bb07687f15b7ecff3f820c51b3dd2cf844cfa555
abderrahmane <ilahalaha0987@gmail.com>
2023-09-15 18:37:35 +0100

    updating the ui

--- src/components/headings/settingList.tsx
+++ src/components/headings/settingList.tsx
@@ -1,15 +1,17 @@
 "use client"
 
 import { useTranslation } from "@/app/i18n/client"
+import { cn } from "@/lib/tailwind"
 import { SecondaryBtn } from "@/ui/buttons"
+import { H4 } from "@/ui/typography"
 import React, { useState } from "react"
 
 
 
 export const SettingsList : React.FC = ()=>{
-return     <div className="flex flex-col items-center" >
-    <div>Saved Cities</div> 
-    <div>Your Notes</div>
+return     <div className="flex gap-2 py-2 px-2 flex-col items-center" >
+    <div style={{borderWidth : '0.5px'}} className="py-1  px-2 border-black w-full " ><H4>Saved Cities</H4></div> 
+    <div style={{borderWidth : '0.5px'}} className="py-1  px-2   border-black w-full " ><H4> Your Notes</H4></div>
     </div>
 
 }
@@ -17,7 +19,7 @@ return     <div className="flex flex-col items-center" >
 export const Settings = ()=>{
 const {t} = useTranslation()
 const [OpenList, setOpenList] = useState(true)
-    return <div className="relative z-20 flex-col bg-white " > <SecondaryBtn onClick={()=>setOpenList((val)=>{ console.log("val" , !val)  ;  return  !val})}  >  {t("loggedHeader.settings")}   <i className="bi bi-gear-fill"></i> </SecondaryBtn>
-    {OpenList &&   <SettingsList/>  }
+    return <div  className={cn("relative rounded-t-3xl  rounded-b-xl  flex-col  " , {  "border-2 border-black bg-white"  :OpenList  } )}> <SecondaryBtn  className="w-full px-5"  onClick={()=>setOpenList((val)=>  !val)}  >  {t("loggedHeader.settings")}   <i className="bi bi-gear-fill"></i> </SecondaryBtn>
+    <div className={cn({"visible" : OpenList , "invisible" : !OpenList })} >  <SettingsList/>  </div>
     </div>
 }
