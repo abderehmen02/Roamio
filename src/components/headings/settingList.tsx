d52a410f17f3775a7eecf7a0d34686642e188114
abderrahmane <ilahalaha0987@gmail.com>
2023-09-15 20:31:44 +0100

    optmizing the settings width ui

--- src/components/headings/settingList.tsx
+++ src/components/headings/settingList.tsx
@@ -10,8 +10,8 @@ import React, { useState } from "react"
 
 export const SettingsList : React.FC = ()=>{
 return     <div className="flex gap-2 py-2 px-2 flex-col items-center" >
-    <div style={{borderWidth : '0.5px'}} className="py-1  px-2 border-black w-full " ><H4>Saved Cities</H4></div> 
-    <div style={{borderWidth : '0.5px'}} className="py-1  px-2   border-black w-full " ><H4> Your Notes</H4></div>
+    <div  className="py-1 cursor-pointer hoverSettingsList hover:leading-8 thinBorderBottom  px-2 border-black w-full " ><H4 className="w-full text-center" >Saved Cities</H4></div> 
+    <div  className="py-1 cursor-pointer hoverSettingsList thinBorderBottom px-2   border-black w-full " ><H4 className="w-full text-center" > Your Notes</H4></div>
     </div>
 
 }
@@ -19,7 +19,8 @@ return     <div className="flex gap-2 py-2 px-2 flex-col items-center" >
 export const Settings = ()=>{
 const {t} = useTranslation()
 const [OpenList, setOpenList] = useState(true)
-    return <div  className={cn("relative rounded-t-3xl  rounded-b-xl  flex-col  " , {  "border-2 border-black bg-white"  :OpenList  } )}> <SecondaryBtn  className="w-full px-5"  onClick={()=>setOpenList((val)=>  !val)}  >  {t("loggedHeader.settings")}   <i className="bi bi-gear-fill"></i> </SecondaryBtn>
+    return <div  className={cn("relative rounded-t-3xl  rounded-b-xl  flex-col  " , {  "border-2 border-black bg-white"  :OpenList  } )}> 
+    <SecondaryBtn  className="w-full px-5"  onClick={()=>setOpenList((val)=>  !val)}  >  {t("loggedHeader.settings")}   <i className="bi bi-gear-fill"></i> </SecondaryBtn>
     <div className={cn({"visible" : OpenList , "invisible" : !OpenList })} >  <SettingsList/>  </div>
     </div>
 }
