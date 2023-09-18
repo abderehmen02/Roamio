b26cebe1869f12052fd276f48eb080002e6e76ec
abderrahmane <ilahalaha0987@gmail.com>
2023-09-18 16:48:49 +0100

    writing script to add modern cities in the database

--- src/app/api/(cities)/addCities/route.ts
+++ src/app/api/(cities)/addCities/route.ts
@@ -1,20 +1,25 @@
 import { cityModal } from "@/db/models/city"
 import { apiResponse } from "@/utils/api/nextResponse"
 import { asyncWrapperApi } from "@/utils/asyncWrapper"
-import { mostVisited } from "./static"
+import {  ModernCities } from "./static"
 import { Categories } from "@/types/prefrences"
 import { StatusCodes } from "http-status-codes"
 
+
+
+
+
 export const GET = asyncWrapperApi(async ()=>{
-   let updatedCities = 0 ;
-      for(let i  = 0 ; i < mostVisited.length ; i++){
-      const existCity = await cityModal().findOne({name : mostVisited[i].name})
-      if(existCity){ await cityModal().findOneAndUpdate({name : mostVisited[i].name} , { ...mostVisited[i] , landmarks : mostVisited[i].landmarks.map(landmark =>({name :landmark , likes: [] , dislikes: []  , reviews: [] })) , reviews: [] , likes: [] , dislikes : [] , categories:existCity.categories} ,{new: true})
-      updatedCities++
-console.log("updated city" , updatedCities)
+      let addedCities = 0 ;
+         for(let i  = 0 ; i < ModernCities.length ; i++){
+         const existCity = await cityModal().findOne({name : ModernCities[i].name})
+         if(existCity){ await cityModal().findOneAndUpdate({name : ModernCities[i].name} , { $addToSet: { categories: Categories.Modern  }} ,{new: true})
+         }
+         else{ await cityModal().create({...ModernCities[i] , landmarks : ModernCities[i].landMarks.map(landmark=>({name :landmark , likes: [] , dislikes: []  , reviews: [] }) ) , categories: [Categories.Modern]  , reviews: [] , likes: [] , dislikes : []  })
+         addedCities++ 
+
       }
-      // else await cityModal().create({...mostVisited[i] , categories: [Categories.MostVisited]})
-      }
-      const newCities = await cityModal().find({})
-      return apiResponse(StatusCodes.CREATED , JSON.stringify(newCities))      
-})
+         }
+         const newCities = await cityModal().find({})
+         return apiResponse(StatusCodes.CREATED , JSON.stringify({ data: newCities , number : addedCities } ))      
+   })
