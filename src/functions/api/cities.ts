e7a5cbd459920de1fe8a4e4e16c272f8cc6ba980
abderrahmane <ilahalaha0987@gmail.com>
2023-09-24 15:21:54 +0100

    updating the getCity function

--- src/functions/api/cities.ts
+++ src/functions/api/cities.ts
@@ -1,11 +1,12 @@
 import { CityDb } from "@/db/models/city"
+import { QueryObjParams } from "@/utils/queryCities"
 // import { generateQueryCitiesSearchParam } from "@/utils/queryCities"
 import axios from "axios"
 
 
 export const getCity = async (city : string) : Promise<CityDb>=>{
-console.log("city" , city )
-// const query = generateQueryCitiesSearchParam({name : city})
-const response = await axios.get(`/api/getCities`)
+const urlSearchParams = new URLSearchParams()
+urlSearchParams.set(QueryObjParams.name , city )
+const response = await axios.get(`/api/getCities?${urlSearchParams.toString()}` )
 return response.data[0] as CityDb
 }
