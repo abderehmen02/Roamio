9c351fea010df38694bd6f0bd1257899cba4adca
abderrahmane <ilahalaha0987@gmail.com>
2023-09-27 10:42:01 +0100

    centering the saved cities on the page

--- src/app/[lang]/(logged)/savedCities/page.tsx
+++ src/app/[lang]/(logged)/savedCities/page.tsx
@@ -19,6 +19,6 @@ if(!isUserInfo(userInfo)) return <H3>it seems that you are not logged in! please
 { userInfo.savedCities?.length ?         <Title title="Your Cities" descreption="Your Collection of Treasured Destinations." /> :   <div className="w-full flex flex-col items-center justify-center gap-5" ><Title title="No city found" descreption="You did not saved any city yet " />
     <Link href="/dashboard" ><PrimaryBtn>Explore Cities</PrimaryBtn></Link>
     </div> }
-     <PageBody>   <SavedCities/></PageBody>
+     <PageBody className="w-full justify-center  " >   <SavedCities/></PageBody>
     </LoggedPage>
 }
