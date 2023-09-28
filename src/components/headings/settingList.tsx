67032f25033bba3f918656ad0e20dafde1f6e13b
abderrahmane <ilahalaha0987@gmail.com>
2023-09-28 10:51:18 +0100

    adding icons in menu list

--- src/components/headings/settingList.tsx
+++ src/components/headings/settingList.tsx
@@ -10,16 +10,17 @@ import Link from "next/link"
 import { useRouter } from "next/navigation"
 import React, { useState } from "react"
 import { useDispatch } from "react-redux"
-
-
+import NotesIcon from '@mui/icons-material/Notes';
+import NotInterestedIcon from '@mui/icons-material/NotInterested';
+import ArticleIcon from '@mui/icons-material/Article';
 
 export const SettingsList : React.FC = ()=>{
     const router= useRouter()
     const dispatch = useDispatch()
 return     <div   className="flex  menu  shadow-2xl   px-4 flex-col items-center" >
-    <Link href={appConfig.links.savedCities}  className="py-3 cursor-pointer hoverSettingsList  thinBorderBottom  px-2 border-black w-full " ><H4 className="w-full text-center" >Saved Cities</H4></Link> 
-    <Link href="/notes" className=" cursor-pointer py-3 hoverSettingsList thinBorderBottom px-2   border-black w-full " ><H4 className="w-full text-center" > Your Notes</H4></Link>
-    <div onClick={()=>logout(dispatch , router.push)} className="py-3  cursor-pointer hoverSettingsList thinBorderBottom px-2   border-black w-full " ><P className="w-full text-center" >Logout</P></div>
+    <Link href={appConfig.links.savedCities}  className="py-3 gap-3  cursor-pointer items-center justify-start hoverSettingsList  thinBorderBottom  flex px-7 border-black w-full " ><NotesIcon/><H4 className=" text-center  w-fit" >Saved Cities</H4>  </Link> 
+    <Link href="/notes" className="flex gap-3 items-center justify-start cursor-pointer py-3 hoverSettingsList thinBorderBottom    border-black w-full px-7" ><ArticleIcon/><H4 className="w-fit text-center" > Your Notes</H4></Link>
+    <div onClick={()=>logout(dispatch , router.push)} className="py-3 flex items-center justify-start gap-3  cursor-pointer hoverSettingsList thinBorderBottom px-7   border-black w-full " ><NotInterestedIcon/><P className="w-fit text-center" >Logout</P></div>
     </div>
 
 }
@@ -27,8 +28,8 @@ return     <div   className="flex  menu  shadow-2xl   px-4 flex-col items-center
 export const Settings = ()=>{
 const {t} = useTranslation()
 const [OpenList, setOpenList] = useState(false)
-    return <div  className={cn("w items-center w-64 rounded-t-3xl flex gap-1 relative rounded-b-xl  flex-col  "  )}> 
-    <SecondaryBtn  className="w-full px-5 "  onClick={()=>setOpenList((val)=>  !val)}  >  {t("loggedHeader.settings")}   <i className="bi bi-gear-fill"></i> </SecondaryBtn>
+    return <div  className={cn("items-center w-64 rounded-t-3xl flex gap-2 relative rounded-b-xl  flex-col  "  )}> 
+    <SecondaryBtn  className="w-full px-5  "  onClick={()=>setOpenList((val)=>  !val)}  >  {t("loggedHeader.settings")}   <i className="bi bi-gear-fill"></i> </SecondaryBtn>
     <div  className={cn({"visible bg-white w-full rounded-xl  mx-3" : OpenList , "invisible" : !OpenList })} >  <SettingsList/>  </div>
     </div>
 }
