afc0608e2bb5c7d74dd51c52794cbdffaa9fab7c
abderrahmane <ilahalaha0987@gmail.com>
2023-10-01 12:33:59 +0100

    integrating the send contact message

--- src/app/[lang]/(marketing)/contact/page.tsx
+++ src/app/[lang]/(marketing)/contact/page.tsx
@@ -6,19 +6,36 @@ import { PrimaryInput } from "@/ui/input";
 import { Title } from "@/ui/title";
 import { P } from "@/ui/typography";
 import { InputBase, TextField } from "@mui/material";
+import axios from "axios";
+import { StatusCodes } from "http-status-codes";
 import { useState } from "react";
 
 export default function ContactPage (){
 const [message, setMessage] = useState<string>("")
-const [name, setname] = useState<string>("")
+const [name, setName] = useState<string>("")
 const [email, setEmail] = useState<string>("")
+const [sendingMessage, setSendingMessage] = useState<boolean>(false)
+const [messageSent, setMessageSent] = useState<boolean>(false)
+const [messageError, setMessageError] = useState(false)
+const sendContactMessage = async () : Promise<void> =>{
+    setSendingMessage(true)
+    const res = await axios.post("/api/contact" , {
+        message , 
+        name , 
+        email
+    })
+    setSendingMessage(false)
+    if(res.status !== StatusCodes.CREATED) return  setMessageError(true)
+    setMessageSent(true) 
+}
+
 
 return <div className="flex flex-col items-center gap-12 justify-center min-h-screen">
 <Title title="Contact Us" descreption="Have a proplem? leave us a message and we will get back to you asap" />
-<form className="flex gap-11 rounded-lg px-24 py-11 flex-col shadow-lg bg-white" >
-<PrimaryInput className="w-[550px]" label="name" />
-<PrimaryInput className="w-[550px]"  label="email" type="email" required />
-<TextField  label="message" placeholder="Type your message" variant="filled" multiline maxRows={5} minRows={3} />
+<form onSubmit={(sendContactMessage)} className="flex gap-11 rounded-lg px-24 py-11 flex-col shadow-lg bg-white" >
+<PrimaryInput value={name} onChange={(e)=>setName(e.target.value)} className="w-[550px]" label="name" />
+<PrimaryInput value={email} onChange={(e)=>setEmail(e.target.value)} className="w-[550px]"  label="email" type="email" required />
+<TextField  value={message} onChange={(e)=>setMessage(e.target.value)} label="message" placeholder="Type your message" variant="filled" multiline maxRows={5} minRows={3} />
 <PrimaryBtn type="submit"  className="flex items-center justify-center gap-2"><P>Send</P><i className="bi bi-send"></i></PrimaryBtn>
 </form>
     </div>
