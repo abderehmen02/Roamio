378fc8ab6a244e447f328b9f31a2bba09649831c
abderrahmane <ilahalaha0987@gmail.com>
2023-10-03 12:18:51 +0100

    integrating the layout in the find cities

---
+++ src/app/[lang]/findCities/layout.tsx
@@ -0,0 +1,12 @@
+import { Footer } from "@/components/layout/footer";
+import { UnlogedHeader } from "@/components/layout/unlogedHeader";
+import React from "react";
+
+export default function BlogLayout ({children} : {children: React.ReactNode} ){
+    return  <div  className="flex  w-full px-0 flex-col  gap-4" >
+    <UnlogedHeader/>
+
+{children}
+<Footer/>
+</div>
+}
