588beee5a659bb9f6cad46e45aee8c66aece05fa
abderrahmane <ilahalaha0987@gmail.com>
2023-10-03 16:44:36 +0100

    integrating the app config links in the unloged header

--- src/components/layout/unlogedHeader.tsx
+++ src/components/layout/unlogedHeader.tsx
@@ -4,6 +4,7 @@ import { H3 } from "@/ui/typography"
 import Link from "next/link"
 import "bootstrap-icons/font/bootstrap-icons.css"
 import { useTranslation } from "@/app/i18n"
+import { appConfig } from "@/config"
 
 export  async function  UnlogedHeader  (){
     const {t} = await useTranslation()
@@ -11,10 +12,10 @@ export  async function  UnlogedHeader  (){
     return(
         <div className="flex w-full items-center justify-around " >
 <img src="/logo-color.png" className="w-16 h-16" />
-<Link href="/blog" > <H3 className="text-primary " > {t("unlogedHeader.blog")}</H3></Link>
-<Link href="/findCity" ><H3 className="text-primary" >{t("find city")}</H3></Link>
-<Link href="/login" > <PrimaryBtn>{t("unlogedHeader.signIn")} <i className="bi bi-person"></i> </PrimaryBtn></Link>
-<Link href="/signUp" > <SecondaryBtn>{t("unlogedHeader.signUp")} <i className="bi bi-person-add"></i> </SecondaryBtn></Link>
+<Link href={appConfig.links.blog} > <H3 className="text-primary " > {t("unlogedHeader.blog")}</H3></Link>
+<Link href={appConfig.links.findCities} ><H3 className="text-primary" >{t("find city")}</H3></Link>
+<Link href={appConfig.links.login}> <PrimaryBtn>{t("unlogedHeader.signIn")} <i className="bi bi-person"></i> </PrimaryBtn></Link>
+<Link href={appConfig.links.signUp}> <SecondaryBtn>{t("unlogedHeader.signUp")} <i className="bi bi-person-add"></i> </SecondaryBtn></Link>
         </div>
     )
 }
