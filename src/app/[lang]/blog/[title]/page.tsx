8c5ac901d04b1199f09d871cfbb3321dc56a805b
abderrahmane <ilahalaha0987@gmail.com>
2023-10-07 10:59:14 +0100

    commpleting the blog post page

--- src/app/[lang]/blog/[title]/page.tsx
+++ src/app/[lang]/blog/[title]/page.tsx
@@ -1,18 +1,34 @@
 import { blogPosts } from "@/constants/blog/blog"
 import { Title } from "@/ui/title"
-import { H2 } from "@/ui/typography"
+import { H2, H3, P } from "@/ui/typography"
 import { ErrorContainer } from "@/ui/errors"
 import { Page } from "@/ui/containers"
 
 
 
  const BlogPage = ({params: {title}} : {params :{title: string}})=>{
-const blogPostInfo = blogPosts.find(post=>post.title === title)
-if(!blogPostInfo) return    <Page className="bg-red-300 w-full hidden" >
+
+    const decodedTitle = decodeURIComponent(title)
+    const blogPostInfo = blogPosts.find(post=>post.title === decodedTitle)
+
+if(!blogPostInfo) return    <Page className="items-center justify-center" >
  <ErrorContainer title="Sorry!" descreption="can not find a post with the specified title" />
  </Page>
 return <Page>
-    <Title title={blogPostInfo?.title} />
+    <Title title={blogPostInfo?.title} descreption={blogPostInfo.descreption} />
+    <img style={{height : '500px', width : '80%'  }} src={blogPostInfo.image} />
+<div style={{width : '80%'}} className="flex flex-col gap-5" >    {
+       blogPostInfo.articles.map(blogPost=><div >
+            <H2>{blogPost.title}</H2>
+            {
+                blogPost.paragraphs.map(paragraph=>{
+             return        <P>
+                        {paragraph}
+                    </P>
+                })
+            }
+        </div>)
+    }</div>
 </Page>
 }
 
