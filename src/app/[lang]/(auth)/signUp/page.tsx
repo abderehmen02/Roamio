eb94ab34e8a1361059b08b266fecd2ea663b1598
abderrahmane <ilahalaha0987@gmail.com>
2023-10-07 14:45:14 +0100

    integrating the page component in the sign up page

--- src/app/[lang]/(auth)/signUp/page.tsx
+++ src/app/[lang]/(auth)/signUp/page.tsx
@@ -2,14 +2,15 @@ import { Title } from "@/ui/title";
 import { SignUpCards } from "./_components/cards";
 import { SignUpForm } from "./_components/signUpForm";
 import { useTranslation } from "@/app/i18n";
+import { Page } from "@/ui/containers";
 
 export  default  async function  SignUpPage(){
     const {t} = await useTranslation()
-    return <div  className="flex items-center flex-col" >
+    return <Page  className="flex items-center flex-col" >
         <Title titleClassName="text-5xl" className="my-12" title={t("appName")} descreptionClassName="font-semibold" descreption={t("signUp.titleDescreption")} />
         <div className="bg-secondary relativ w-fit e items-center p-16 rounded-xl gap-10 justify-center flex" >
      <SignUpForm/>
             <SignUpCards/>
         </div>
-    </div>
+    </Page>
 }
