2ea31087a09a5b41a5a697d6ec68d0f56934c610
abderrahmane <ilahalaha0987@gmail.com>
2023-10-10 12:57:21 +0100

    integrating the loading page

---
+++ src/app/[lang]/loading.tsx
@@ -0,0 +1,8 @@
+import { appConfig } from "@/config";
+import { H2, H3 } from "@/ui/typography";
+
+export default function LoadingPage (){
+    return <div  className="h-screen w-screen flex items-center flex-col bg-pageBg justify-center" >
+    <H2>Loading...</H2>
+    </div>
+}
