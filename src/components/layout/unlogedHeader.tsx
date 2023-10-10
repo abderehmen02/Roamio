b440e1f406984fca4c743e2c86e73b9c7b6fb7e6
abderrahmane <ilahalaha0987@gmail.com>
2023-10-10 16:35:27 +0100

    blog page is responsive

--- src/components/layout/unlogedHeader.tsx
+++ src/components/layout/unlogedHeader.tsx
@@ -16,14 +16,14 @@ export  async function  UnlogedHeader  (){
     const {t} = await useTranslation()
     
     return(<>
-        <div  className="hidden laptop:flex w-full items-center justify-around " >
+        <div  className="hidden laptop:flex w-full  items-center justify-around " >
 <Link href={appConfig.links.home} ><img src={appConfig.logoImage} className="w-16 h-16" /></Link>
 <Link href={appConfig.links.blog} > <H3 className="text-primary " > {t("unlogedHeader.blog")}</H3></Link>
 <Link href={appConfig.links.findCities} ><H3 className="text-primary" >{t("find city")}</H3></Link>
 <Link href={appConfig.links.login}> <PrimaryBtn>{t("unlogedHeader.signIn")} <i className="bi bi-person"></i> </PrimaryBtn></Link>
 <Link href={appConfig.links.signUp}> <SecondaryBtn>{t("unlogedHeader.signUp")} <i className="bi bi-person-add"></i> </SecondaryBtn></Link>
         </div>
-        <div className="flex laptop:hidden" >
+        <div className="flex w-full laptop:hidden" >
             <UnlogedMobileHeader/>
         </div>
         </>
