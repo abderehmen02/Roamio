4ff9b50531cce734adcb40733b79ee26fd909f33
abderrahmane <ilahalaha0987@gmail.com>
2023-10-20 19:39:55 +0100

    editing the loading page

--- src/app/[lang]/loading.tsx
+++ src/app/[lang]/loading.tsx
@@ -1,8 +1,10 @@
 import { appConfig } from "@/config";
 import { H2, H3 } from "@/ui/typography";
+import { Box, LinearProgress } from "@mui/material";
 
 export default function LoadingPage (){
     return <div  className="h-screen w-screen flex items-center flex-col bg-pageBg justify-center" >
-    <H2>Loading...</H2>
+    <img src={appConfig.logoImage} style={{width  : '100px' , height : "100px"}} />
+    <Box sx={{width : '100px'}} ><LinearProgress/></Box>
     </div>
 }
