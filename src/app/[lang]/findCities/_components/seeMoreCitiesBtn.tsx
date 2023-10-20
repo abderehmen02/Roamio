52e1a865cc2bf0bb50761641700a6df99341c5a3
abderrahmane <ilahalaha0987@gmail.com>
2023-10-20 17:24:21 +0100

    hiding next and previous button conditionaly

--- src/app/[lang]/findCities/_components/seeMoreCitiesBtn.tsx
+++ src/app/[lang]/findCities/_components/seeMoreCitiesBtn.tsx
@@ -1,4 +1,5 @@
 import { appConfig } from "@/config";
+import { cn } from "@/lib/tailwind";
 import ActionCreators from "@/state/actionCreators/action";
 import { stateType } from "@/state/reducers";
 import { CitiesQueryActionTypes } from "@/types/state/citiesQuery";
@@ -14,12 +15,13 @@ import { bindActionCreators } from "redux";
 export const SeeMoreCities : React.FC = ()=>{
     const searchParams = useSearchParams()!
     const currentPage : string = searchParams.get(QueryObjParams.page) || "1"
+    const pageEnd : boolean = Boolean(searchParams.get(QueryObjParams.endPage) === "true" ) 
     const router = useRouter()
     const pathname = usePathname()
     const citiesState  = useSelector((state : stateType)=>state.cities)
     const dispatch = useDispatch()
     const {dispatchAction} = bindActionCreators(ActionCreators , dispatch )
-
+    
 const nextPage = () : void =>{
      const urlSearchParams = new URLSearchParams(searchParams.toString() )
       urlSearchParams.set(QueryObjParams.page, String(Number(currentPage) +  1))        
@@ -38,8 +40,8 @@ const prevPage = () : void =>{
     if(citiesState.cities.length === 0 ) return <></>
     return <div className="w-full" >
 { citiesState.loading  ? <span>loading...</span> : <div className="flex w-full justify-between items-center" >
-     <SecondaryBtn onClick={prevPage} >Previous <i className="bi mx-3 text-2xl bi-arrow-left-circle-fill"></i> </SecondaryBtn>
-     <PrimaryBtn onClick={nextPage} >Next <i className="bi text-2xl mx-3 bi-arrow-right-circle-fill"></i> </PrimaryBtn>  
+{     <SecondaryBtn onClick={prevPage} className={cn({"invisible " : Number(currentPage) < 2 })} >Previous <i className="bi mx-3 text-2xl bi-arrow-left-circle-fill"></i> </SecondaryBtn> }
+{     <PrimaryBtn onClick={nextPage} className={cn({"invisible" : pageEnd})} >Next <i className="bi text-2xl mx-3 bi-arrow-right-circle-fill"></i> </PrimaryBtn>  }
      </div> }
      </div>
 }
