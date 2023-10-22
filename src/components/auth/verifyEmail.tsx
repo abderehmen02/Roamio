434d5dff6d391e830adaba6882493dde0d95bcfd
abderrahmane <ilahalaha0987@gmail.com>
2023-10-23 00:00:30 +0100

    integrating resend email functionality

--- src/components/auth/verifyEmail.tsx
+++ src/components/auth/verifyEmail.tsx
@@ -2,24 +2,43 @@ import { appConfig } from "@/config";
 import { logout } from "@/functions/api/auth";
 import { stateType } from "@/state/reducers";
 import { isUserInfo } from "@/types/state/auth/userInfo";
-import { ButtonsSizes, PrimaryBtn } from "@/ui/buttons";
+import { ButtonsSizes, PrimaryBtn, SecondaryBtn } from "@/ui/buttons";
 import { H4 } from "@/ui/typography";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-
+import {toast} from "sonner"
 
 export const VerifyEmail : React.FC = ()=>{
 const dispatch = useDispatch()
 const router  = useRouter()
 const userInfo = useSelector((state : stateType)=>state.userInfo)
 
+const {data , isError , error , isLoading } = useMutation({
+mutationFn : async ()=>{
+const responce = await  axios.post("/api/sendVerifyMessage")
+return responce.data
+},
+onSuccess : ()=>{
+    toast.success("Message has been sent!")
+},
+onError : ()=>{
+    toast.error("Something went wrong! please try again.")
+}
+})
+
+
 // if(!isUserInfo(userInfo)) return   logout(dispatch  , router.push , appConfig.links.login)
 
      return <div className="flex items-center px-2 gap-5 jusitfy-center py-12 flex-col" >
     <H4  className="text-center" >We sent a message to your email. please check and verify your email: {isUserInfo(userInfo) && userInfo.email} </H4>
-    <PrimaryBtn size={ButtonsSizes.medium} onClick={()=>logout(dispatch  , router.push , appConfig.links.login)} >Log Out</PrimaryBtn>
+    <div>
+    <SecondaryBtn size={ButtonsSizes.medium} onClick={()=>logout(dispatch  , router.push , appConfig.links.login)} >Log Out</SecondaryBtn>
+    <PrimaryBtn loading={isLoading} >Resend Verify Message</PrimaryBtn>
+    </div>
     </div>
 }
 
