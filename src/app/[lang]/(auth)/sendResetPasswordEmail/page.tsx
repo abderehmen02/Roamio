432ab4e9ce3197dc04a9fb26291f0116704b539d
abderrahmane <ilahalaha0987@gmail.com>
2023-10-24 16:36:42 +0100

    reseting email is working

--- src/app/[lang]/(auth)/sendResetPasswordEmail/page.tsx
+++ src/app/[lang]/(auth)/sendResetPasswordEmail/page.tsx
@@ -6,9 +6,11 @@ import { Page } from "@/ui/containers"
 import { PrimaryInput } from "@/ui/input"
 import { Title } from "@/ui/title"
 import { H3 } from "@/ui/typography"
+import { SendResetPasswordEmailData } from "@/utils/validators/auth"
 import { useMutation } from "@tanstack/react-query"
 import axios from "axios"
 import { useRouter } from "next/navigation"
+import { useState } from "react"
 import { useForm } from "react-hook-form"
 import { toast } from "sonner"
 
@@ -17,25 +19,29 @@ import { toast } from "sonner"
 
 
 export default function ResetPasswordPage  ({params : {token} } : {params : {token  : string}} ){
-const {register , handleSubmit }  = useForm<{newPassword : string}>()
+const [firstEmailSent, setFirstEmailSent] = useState(false)
+const {register , handleSubmit }  = useForm<SendResetPasswordEmailData>()
 const router = useRouter()
 const { isLoading ,isError , mutate } = useMutation({
-    mutationFn : async (data : {newPassword: string} )=>{
+    mutationFn : async (data : SendResetPasswordEmailData )=>{
         const response = await axios.post("/api/sendResetPasswordEmail" , data)
         return response.data
     } ,
     onSuccess : ()=>{
-        toast.success("password has been reset succussfully! you can log in with the new password")
-        router.push(appConfig.links.login)
+        toast.success("email sent!")
+        setFirstEmailSent(true)
+    },
+    onError : ()=>{
+        toast.error("something went wrong! please try again")
     }
 })
 
 
 return <Page >
-<form onSubmit={handleSubmit((data)=>mutate)}  className="flex flex-col gap-10 items-center" >
-<Title title="Reset Your Password"  descreption="Please enter your email or username" />
-<PrimaryInput label="Your email or username" type="text" placeholder="Type a new password" {...register("newPassword")} />
-<PrimaryBtn  type="submit" loading={isLoading} >Send Email</PrimaryBtn>
+<form onSubmit={handleSubmit((data)=>mutate(data))}  className="flex flex-col gap-10 items-center" >
+<Title title="Your Email Or Username"  descreption={ firstEmailSent ? "Email has been sent! please go to your email and follow the link that we sent to create a new passowrd"  :  "Plase type your email or username to send you a reset password email"} />
+<PrimaryInput label="Your email or username" type="text" placeholder="Type a new password" {...register("userIdentifier")} />
+<PrimaryBtn  type="submit" loading={isLoading} > { firstEmailSent ? "Resend Email" :  "Send Email" }</PrimaryBtn>
 </form>
     </Page>
 }
--- src/app/api/(auth)/sendResetPasswordEmail/route.ts
+++ src/app/api/(auth)/sendResetPasswordEmail/route.ts
@@ -1,20 +1,43 @@
 import { authConfig } from "@/config/auth";
-import { userModel } from "@/db/models/user";
+import { UserDb, userModel } from "@/db/models/user";
 import { ResetPasswordTokenInfo, TokenTypes } from "@/types/auth/token";
 import { errorMessage } from "@/utils/api/error";
 import { apiResponse } from "@/utils/api/nextResponse";
 import { asyncWrapperApi } from "@/utils/asyncWrapper";
 import { sendResetPasswordEmail, sendVerifyEmail } from "@/utils/auth/email";
 import { verifyLoginToken } from "@/utils/auth/tokens";
+import { sendResetPasswordEmailValidator } from "@/utils/validators/auth";
 import ResponseStatuses, { StatusCodes } from 'http-status-codes'
 import { redirect } from "next/navigation";
 
 
-export const GET = asyncWrapperApi( async (req)=>{
-const email = req.headers.get("email")
-const  userDb = await userModel().findOne({email })
+const isEmail = (email : string) => {
+    return String(email )
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+
+export const POST = asyncWrapperApi( async (req)=>{
+    console.log("getting a request")
+const body = await req.json()
+const parsedBody = sendResetPasswordEmailValidator.safeParse(body)
+console.log("parsed body" ,parsedBody )
+if(!parsedBody.success) return apiResponse(StatusCodes.BAD_REQUEST , errorMessage("Can not get the user identifier"))
+let userDb : UserDb | null ;
+const userIdentifier = parsedBody.data.userIdentifier
+if(isEmail(userIdentifier)){
+userDb  = await userModel().findOne({email : userIdentifier }) 
+}
+else {
+userDb = await userModel().findOne({userName : userIdentifier})    
+}
+console.log("user db" , userDb)
 if(!userDb)return apiResponse(ResponseStatuses.NOT_FOUND  , errorMessage("can not find user . "))
 const emailSent =  await sendResetPasswordEmail(userDb)   
-if(emailSent === true) redirect("/resetPasswordEmailSent")
+console.log("email sent"  , emailSent)
+if(emailSent === true) return apiResponse(StatusCodes.CREATED , {sent : emailSent})
 else return new Response(JSON.stringify(emailSent) , {status : StatusCodes.INTERNAL_SERVER_ERROR} )
 })
