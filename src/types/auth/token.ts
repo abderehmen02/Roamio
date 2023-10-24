5f9152f82d8ac8d8f8ec7d9b4823ef27aa627253
abderrahmane <ilahalaha0987@gmail.com>
2023-10-24 13:57:07 +0100

    integrating the ResetPasswordTokenInfo type

--- src/types/auth/token.ts
+++ src/types/auth/token.ts
@@ -4,3 +4,13 @@ export type UserStoredWithToken = {
 userId: string  , 
 authService? : AuthService
 }
+
+export enum TokenTypes {
+    RESET_PASSWORD = "reset-password" 
+}
+
+
+export interface ResetPasswordTokenInfo {
+    type : TokenTypes.RESET_PASSWORD , 
+    userId : string ,
+}
