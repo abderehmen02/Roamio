d67615db71f16a9e73d9b59fe7598c1e7d7f43b8
abderrahmane <ilahalaha0987@gmail.com>
2023-10-28 12:07:56 +0100

    fixing landmark skeleton issue

--- src/components/skeletons/city/landmarkSkeleton.tsx
+++ src/components/skeletons/city/landmarkSkeleton.tsx
@@ -5,12 +5,12 @@ export const LandmarkSkeletonCard : React.FC =()=>{
 return  <>
 
 {/* skeleton for desktop */}
-<div  className="hidden laptop:flex shadow-md   rounded-xl  w-full border-stone-600 relative" >
+<div  className="hidden laptop:flex shadow-md relative  rounded-xl  w-full border-stone-600 " >
     <Skeleton  variant="rectangular"  style={{width : '200px' , margin : 0 ,  height : '100%' }}    />    
-    {/* <div className="flex px-6 py-2 justify-around flex-col gap-1" >
-     <TitleSkeleton title={true}  titleStyle={{width : 250, height: 40}}  className="flex-row   items-center justify-start gap-7"  descreptionStyle={{width : 500 , height : 30}} descreption={true}  />
+    <div className="flex px-6 py-2 w-full relative justify-around flex-col gap-1" >
+     <TitleSkeleton title={true}  titleStyle={{width : '20%', height: 40}}  className="flex-row w-full relative  items-center justify-start gap-7"  descreptionStyle={{width : "60%" , height : 30}} descreption={true}  />
      <Skeleton variant="rectangular" width="100%" height={100} />
-    </div>     */}
+    </div>    
     </div>
 
 {/* skeleton for mobile */}
