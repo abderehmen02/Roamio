e68f32e979eb4a606826b6f1e570bae7bc10d859
abderrahmane <ilahalaha0987@gmail.com>
2023-11-01 13:35:57 +0100

    fixing logout issue

--- src/app/api/(auth)/signIn/route.ts
+++ src/app/api/(auth)/signIn/route.ts
@@ -14,22 +14,16 @@ import { authConfig } from "@/config/auth";
 
 
 export const POST  = asyncWrapperApi(async (req )=>{
-  console.log("login request")
     const body = await req.json()
     const parsedBodyResult = signInValidator.safeParse(body)
     if(!parsedBodyResult.success) return  apiResponse(StatusCodes.BAD_REQUEST , parsedBodyResult.error )
-    console.log("succuss! parsed")
     const {userName , password} = parsedBodyResult.data
     const user = await userModel().findOne({userName})
-    console.log("user" , user)
     if(!user) return apiResponse(StatusCodes.BAD_REQUEST , errorMessage(signInFieldErrors.noUserFound.shortMessage))
     const isPasswordTrue = await  bycrypt.compare(password , user.password )
-  console.log("password true " , isPasswordTrue )
     if(!isPasswordTrue)  return apiResponse(StatusCodes.BAD_REQUEST , errorMessage(signInFieldErrors.incorrectPassword.shortMessage) ) 
     const token = generateLoginToken({userId : user._id.toString() })
-  console.log("token" , token)
     const refreshToken = await  generateRefreshToken(user._id.toString() )
-      console.log("refresh token" , refreshToken)
     const response =   NextResponse.json(
         {
           ...user._doc,
