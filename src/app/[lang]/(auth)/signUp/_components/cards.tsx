09bcb71bd5d7fb98b02c3cd05b3a3bcbab43a839
abderrahmane <ilahalaha0987@gmail.com>
2023-11-01 09:35:21 +0100

    renaming useTranslation

--- src/app/[lang]/(auth)/signUp/_components/cards.tsx
+++ src/app/[lang]/(auth)/signUp/_components/cards.tsx
@@ -1,9 +1,9 @@
-import { useTranslation } from "@/app/i18n"
+import { useTranslation as translate } from "@/app/i18n"
 import { InfoCard, cardsColors } from "@/components/marketing/cards"
 
 
 export const SignUpCards = async ()=>{ 
-    const {t }  = await  useTranslation()
+    const {t }  = await  translate()
     return <div  className="flex flex-col gap-10 items-center justify-between   " >
     <InfoCard className="shadow-lg" color={cardsColors.secondaryDark} title={t("signUp.cards.firstCardTitle")}   description={t("signUp.cards.firstCardDescreption")}/>
     <InfoCard className="shadow-lg" color={cardsColors.secondaryDark} title={t("signUp.cards.secondCardTitle")}  description={t("signUp.cards.secondCardDescreption")} />
