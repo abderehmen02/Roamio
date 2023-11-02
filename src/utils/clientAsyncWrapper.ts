c04798ce51d8e9aaef79bea94d6584bd2db0cbf6
abderrahmane <ilahalaha0987@gmail.com>
2023-11-02 11:44:58 +0100

    handling error in the async wraper

--- src/utils/clientAsyncWrapper.ts
+++ src/utils/clientAsyncWrapper.ts
@@ -1,15 +1,20 @@
-type AsyncFunction<Args extends any[] = any[], Return = any> = (...args: Args) => Promise<Return>;
+import { StatusCodes } from "http-status-codes";
+import { apiResponse } from "./api/nextResponse";
+import { errorMessage } from "./api/error";
+
+type AsyncFunction<Args extends any[] = any[], Return = any> = (...args: Args) => Promise<Return  >;
 
 export function asyncWrapper<Args extends any[], Return>(
-  fn: AsyncFunction<Args, Return>
-): (...args: Args) => Promise<Return |  void> {
+  fn: AsyncFunction<Args, Return  >
+): (...args: Args) => Promise<Return |  void | Response > {
   return async function (...args: Args) {
     try {
       const result = await fn(...args);
       return result;
-    } catch (error) {
+    } catch (error : any ) {
       // You can handle or log errors here if needed
-       console.error(error)
-    }
+       console.error(error)    
+      return apiResponse(StatusCodes.INTERNAL_SERVER_ERROR , errorMessage(error))
+      }
   };
 }
