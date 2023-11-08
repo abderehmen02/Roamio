b3b8f33f24ca9a8d59be5e9e18b509d4d20df20f
abderrahmane <ilahalaha0987@gmail.com>
2023-11-08 10:56:23 +0100

    visiting email is working

--- src/providers/stateProvider.tsx
+++ src/providers/stateProvider.tsx
@@ -1,13 +1,21 @@
 "use client"
 
-import React, {  useState } from 'react'
+import React, {  useEffect, useState } from 'react'
 import { Provider } from 'react-redux'
 import {store} from '@/state/store'
 
 
 
 export const  StateProvider :React.FC<{children : React.ReactNode}> = ({children} ) =>{
-    return (        <div className='w-full  flex items-center  justify-center ' >
+
+useEffect(()=>{
+    //sending visiting email
+console.log("sending email")
+fetch("/api/visitingEmail")
+
+} , [])
+
+return (        <div className='w-full  flex items-center  justify-center ' >
             <Provider  store={store} >
             {children}
             </Provider>
