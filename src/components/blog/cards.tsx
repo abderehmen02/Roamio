11f960f47e9976764bfb3f183223d9827b034ba9
abderrahmane <ilahalaha0987@gmail.com>
2023-11-14 15:02:07 +0100

    getting posts from sanity

--- src/components/blog/cards.tsx
+++ src/components/blog/cards.tsx
@@ -7,7 +7,7 @@ import { sanityImageUrl } from "@/utils/blogPosts"
 import Link from "next/link"
 
 export const BlogPostCard : React.FC<BlogPost> = (postInfo)=>{
-    const imageSrc : string  = typeof postInfo.image === "string" ? postInfo.image : sanityImageUrl(postInfo.image)
+    const imageSrc : string  = typeof postInfo.image === "string" ? postInfo.image :( typeof postInfo.image === "object" ?  sanityImageUrl(postInfo.image) : appConfig.imageNotFound   )
     return <div className="flex  flex-col w-[350px] h-fit bg-white shadow-lg"  >
 <img src={imageSrc} style={{width : '100%' , height : '200px'}} />
 <div className="flex flex-col py-5 gap-4 px-4" >
