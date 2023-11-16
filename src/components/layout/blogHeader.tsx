041a7e4f6b6b8f581e294886777e1c23ce44270c
abderrahmane <ilahalaha0987@gmail.com>
2023-11-16 16:23:44 +0100

    fixing the blog header

--- src/components/layout/blogHeader.tsx
+++ src/components/layout/blogHeader.tsx
@@ -12,10 +12,16 @@ import { useTranslation as translate } from "@/app/i18n"
 
 export const BLogHeader = async  ()=>{
     const {t} = await translate()
-    return      <div  className="  fixed top-0 flex w-full  items-center justify-between px-1 " >
+    return      <>
+    <div  className="hidden  laptop:flex w-full  items-center justify-between px-1 " >
 <Link href={appConfig.links.home} ><img alt={appConfig.name + "logo"} src={appConfig.logoImage} className="w-16 h-16" /></Link>
-<div className="flex items-center justify-center gap-11" >
-<Link href={appConfig.links.home}> <PrimaryBtn  >Back To Home </PrimaryBtn></Link>
-</div>
-       </div>
+<Link href={appConfig.links.home}> <PrimaryBtn>Back To Home </PrimaryBtn></Link>
+    </div>
+    <div className="flex w-full laptop:hidden" >
+    <div  className="flex w-full z-30 items-center fixed   top-0 justify-between" >
+<Link href={appConfig.links.home} > <img src={appConfig.logoImage} className="w-16 h-16"  /></Link>
+<Link href={appConfig.links.home} ><PrimaryBtn >Back To Home</PrimaryBtn></Link>
+    </div>
+    </div>
+    </>
 }
