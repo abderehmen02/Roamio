76e6d01f4ee6a15865d555f7463687fa9f107908
abderrahmane <ilahalaha0987@gmail.com>
2023-11-18 16:31:41 +0100

    switching from png to webp

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,7 +10,7 @@ const productionUrl =   process.env.APP_URL
 export const appConfig = {
     dateFormate : 'MM/dd/yyyy' ,
     name : 'Roamio' ,
-    logoImage : "/logo.png" ,
+    logoImage : "/logo.webp" ,
         emailPassword : process.env.GMAIL_PASSWORD ,
     email: process.env.GMAIL_EMAIL ,
     personalEmail : process.env.PERSONAL_EMAIL , 
