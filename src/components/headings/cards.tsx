f0d17e5eced9171ad9778d7bb4e8a0a35336921f
abderrahmane <ilahalaha0987@gmail.com>
2023-11-25 09:54:29 +0100

    fixing profile pic appearence

--- src/components/headings/cards.tsx
+++ src/components/headings/cards.tsx
@@ -48,9 +48,9 @@ export const ProfileCard : React.FC =  ()=>{
     return <></>
 }
     return <div className="flex  flex-col gap-3 px-3 py-6 items-center justify-center" >
-        <div className="relative group  w-[70px] h-[70px] rounded-full  border-2  bg-secondary "  >
-        <AddAPhotoIcon className="absolute opacity-50 group-hover:opacity-100 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
-        <div className="w-fit h-fit bg-white" ><img  className=" w-full h-full rounded-full" src={ userInfo.profilePic || userInfo.picture||  appConfig.unknownPersonImage} /></div>
+        <div className="relative group  w-[70px] h-[70px]  rounded-full  border-2  bg-secondary "  >
+        <AddAPhotoIcon className="absolute bg-white p-1 rounded-full   top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+        <div className="w-full h-full bg-white" ><img  className=" w-full h-full rounded-full object-cover" src={ userInfo.profilePic || userInfo.picture||  appConfig.unknownPersonImage} /></div>
         <input onChange={uploadImage}  type="file" className="w-[70px] h-[70px] absolute top-0 left-0  rounded-full items-center justify-center border-green-600   opacity-0 cursor-pointer" />
         </div>
           <H4>{isGoogleUser(userInfo)  ? userInfo.given_name : userInfo.userName  }</H4> 
