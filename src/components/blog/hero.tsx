eb6bc387e5de4649ff9f1fe29bd52b7555269a84
abderrahmane <ilahalaha0987@gmail.com>
2023-11-26 12:20:01 +0100

    integrating the blogVariants for framer-motion

---
+++ src/components/blog/hero.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { H3, P } from "@/ui/typography"
+import { time } from "console"
+import {motion} from "framer-motion"
+import { useState } from "react"
+
+
+export const blogHeroText : {title : string ,descreption : string}[] = [
+{
+    title  : "Cityscape Chronicles: Dive into Urban Adventures with Our Blogs" ,
+    descreption : "Embark on a captivating journey through city life. Our blogs unveil the essence of cities, showcasing activities, landmarks, and local gems that make each urban landscape unique."
+} ,
+{
+    title : "Urban Pulse: Tune into City Life Through Our Blogs" ,
+    descreption :  "Feel the heartbeat of cities worldwide. Explore our blogs to discover the rhythm of urban living, from iconic landmarks to hidden treasures and vibrant activities." ,
+} ,
+{
+    title : "Wandering Cities: Explore the World's Urban Tapestry in Blogs" ,
+    descreption : "Join us in traversing the intricate patterns of cities. Our blogs weave tales of urban exploration, highlighting landmarks, activities, and the vibrant soul of each locale." ,
+} ,
+{
+    title : "City Unveiled: Dive Deep into Blogs on Urban Wonders" ,
+    descreption :"Peel back the layers of cities with our insightful blogs. Delve into the heart of urban life, discovering landmarks, activities, and the stories that shape these vibrant landscapes."
+}
+]
+
+export const BlogHero = ()=>{
+    useState
+return <div>
+</div>
+}
---
+++ src/constants/blog/animation/framerVariants.ts
@@ -0,0 +1,12 @@
+import { Variants } from "framer-motion";
+
+export const blogVariants : Variants ={
+display : {
+    opacity : 1 ,
+    y : 0
+} ,
+beforeDisplay : {
+    opacity : 0.2 ,
+    y : -5 
+}
+}
