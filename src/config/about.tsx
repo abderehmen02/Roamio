d47603799d812be9aed56950cf1376e286898a6f
abderrahmane <ilahalaha0987@gmail.com>
2024-01-02 17:38:00 +0100

    editing the about features info

--- src/config/about.tsx
+++ src/config/about.tsx
@@ -16,12 +16,12 @@ export const aboutFeatures : AboutFeatureInfo[]  = [
         Icon : <PublicIcon sx={{fontSize : "140px"}} />
     } , 
     {
-        title : "City Details: Explore the Landmarks, Activities, and Information of Each City" ,
+        title : "City Details, Explore the Landmarks, Activities, and Information of Each City" ,
         descreption : "This feature allows you to see the landmarks, activities, and information of each city that you select from the city search feature. You can see the photos, descriptions, and locations of the landmarks, the types, costs, and ratings of the activities, and the general information of the city, such as population, climate, culture, etc. You can also see other people’s comments and likes, and dislike cities and landmarks. This feature helps you to learn more about each city and decide what to do and see there." ,
         Icon : <DirectionsCarIcon  sx={{fontSize : "140px"}} />
     } , 
     {
-        title: "Blog: Read Articles about Cities, Activities, Landmarks, and More" ,
+        title: "Blog, Read Articles about Cities, Activities, Landmarks, and More" ,
         descreption : "This feature allows you to read articles about cities, activities, landmarks, and more on the same app. You can find articles that are relevant to your interests, such as travel tips, cultural insights, historical facts, etc. You can also comment on the articles and share them with your friends. This feature helps you to enrich your knowledge and enjoy your travel experiences." ,
         Icon : <ArticleIcon sx={{fontSize : "140px"}}/>
     }
