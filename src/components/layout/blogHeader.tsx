de5cce2c2add65c1e5a1d7c33024d116fb98f502
abderrahmane <ilahalaha0987@gmail.com>
2024-01-07 12:26:04 +0100

    making the blog header responsive

--- src/components/layout/blogHeader.tsx
+++ src/components/layout/blogHeader.tsx
@@ -1,8 +1,12 @@
+"use client"
 import { appConfig } from "@/config"
 import { PrimaryBtn, SecondaryBtn } from "@/ui/buttons"
 import Link from "next/link"
 import { useTranslation as translate } from "@/app/i18n"
 import { H2, H3 } from "@/ui/typography"
+import { useEffect, useState } from "react"
+import { cn } from "@/lib/tailwind"
+import HomeIcon from '@mui/icons-material/Home';
 
 
 
@@ -10,19 +14,30 @@ import { H2, H3 } from "@/ui/typography"
 
 
 
+export const BLogHeader =   ()=>{
+    const [scrollPosition, setScrollPosition] = useState(0);
 
-export const BLogHeader = async  ()=>{
-    const {t} = await translate()
+    const handleScroll = () => {
+        const position = window.pageYOffset;
+        setScrollPosition(position);
+    };
+    
+    useEffect(() => {
+    typeof window !== "undefined" &&        window.addEventListener('scroll', handleScroll);
+        return () => {
+    typeof window !== "undefined" &&        window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return      <>
     <div  className="hidden   laptop:flex w-full px-12  items-center justify-between py-2" >
 <Link href={appConfig.links.home} ><img alt={appConfig.name + "logo"} src={appConfig.logoImage} className="w-16 h-16" /></Link>
 <Link href={appConfig.links.home}> <PrimaryBtn>Back To Home </PrimaryBtn></Link>
     </div>
     <div className="flex  w-full laptop:hidden" >
-    <div  className="flex w-full z-30 items-center fixed  staticBg top-0 justify-between" >
-<Link href={appConfig.links.home} > <img src={appConfig.logoImage} className="w-16 h-16"  /></Link>
-<Link href={appConfig.links.home} className="mx-2" ><PrimaryBtn >Back To Home</PrimaryBtn></Link>
-    </div>
+    <div  className={cn("flex px-5 py-2 w-full z-30 items-center fixed   top-0 justify-between" , {"bg-white" : scrollPosition >10} )}  >
+<Link href={appConfig.links.home} > <img src={appConfig.logoImage} className="w-12 h-12"  /></Link>
+<HomeIcon sx={{fontSize : "40px"}} />   </div>
     </div>
     </>
 }
