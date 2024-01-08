dd7af1fe2c97843a6442c03c42a7446324057cbb
abderrahmane <ilahalaha0987@gmail.com>
2024-01-08 12:30:29 +0100

    fixing find cities page gap

--- src/app/[lang]/findCities/layout.tsx
+++ src/app/[lang]/findCities/layout.tsx
@@ -3,7 +3,7 @@ import { UnlogedHeader } from "@/components/layout/unlogedHeader";
 import React from "react";
 
 export default function BlogLayout ({children} : {children: React.ReactNode} ){
-    return  <div   className="flex  relative items-center justify-center w-full px-0 flex-col  gap-4 pt-32" >
+    return  <div   className="flex  relative items-center justify-center w-full px-0 flex-col  laptop:gap-4 pt-14 laptop:pt-32" >
     <UnlogedHeader/>
 {children}
 <Footer/>
