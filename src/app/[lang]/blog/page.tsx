b32d4c5566b8c1e5fa6fb53ac69f013c29c30eb3
abderrahmane <ilahalaha0987@gmail.com>
2024-01-09 16:37:43 +0100

    testing

--- src/app/[lang]/blog/page.tsx
+++ src/app/[lang]/blog/page.tsx
@@ -21,7 +21,7 @@ export default async  function BLogPage (query: {
     const sanityPosts = await getSanityPosts() || []
     let allPosts = [...blogPosts  , ...sanityPosts ]
     const category = query?.searchParams && query.searchParams.category
-    console.log(query.searchParams)
+
     // if(category)allPosts =    allPosts.filter(post=>post.category === category)
 return     <Page className="flex items-center  flex-col"  >
         <Title  titleClassName="text-primaryDark"  title="Roamio Blog" descreption={blogPageDescreptions[ Math.floor( Math.random() * 6 )]} />
