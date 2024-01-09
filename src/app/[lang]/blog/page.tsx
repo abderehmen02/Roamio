5017650e030bd72c676656578f3baaa31844c40e
abderrahmane <ilahalaha0987@gmail.com>
2024-01-09 16:09:33 +0100

    disabeling the search params

--- src/app/[lang]/blog/page.tsx
+++ src/app/[lang]/blog/page.tsx
@@ -15,22 +15,22 @@ import { RecommendedForYou } from "./_components/recomanded";
 // export const revalidate = 1
 
 export default async  function BLogPage ({
-    params,
-    searchParams,
+    // params,
+    // searchParams,
   }: {
-    params: { slug: string },
-    searchParams?: { [key: string]: string | string[] | undefined },
+    // params: { slug: string },
+    // searchParams?: { [key: string]: string | string[] | undefined },
   }){
     // const sanityPosts = await getSanityPosts() || []
     let allPosts = blogPosts
-    const category = searchParams && searchParams[appConfig.blog.blogCategoryQueryName]
-    if(category)allPosts =    allPosts.filter(post=>post.category === category)
+    // const category = searchParams && searchParams[appConfig.blog.blogCategoryQueryName]
+    // if(category)allPosts =    allPosts.filter(post=>post.category === category)
 return     <Page className="flex items-center  flex-col"  >
         <Title  titleClassName="text-primaryDark"  title="Roamio Blog" descreption={blogPageDescreptions[ Math.floor( Math.random() * 6 )]} />
         <PageBody className="flex  flex-col" >
         <BlogHero/>
         <DisplayAnimation className="w-full " delay={0.3} >
-        <div className="flex flex-col laptop:flex-row w-full  justify-between  gap-4 " ><BlogFilterCard category={ searchParams && searchParams[appConfig.blog.blogCategoryQueryName]  as string | undefined} />
+        <div className="flex flex-col laptop:flex-row w-full  justify-between  gap-4 " ><BlogFilterCard category={ undefined} />
              <div key="bostsArrayInTheBlogPage" className="flex gap-2  flex-row items-center justify-center w-fit laptop:w-full flex-wrap" >{allPosts.map((post , index )=><DisplayAnimation className="w-fit" key={post.title} delay={(index  + 2) * 0.2} ><BlogPostCard  key={post.title} {...post} /></DisplayAnimation>)}</div>
         </div>
         </DisplayAnimation>
