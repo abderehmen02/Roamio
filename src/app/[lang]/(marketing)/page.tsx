084d40ac9725b79941020089fa47638ced28c2ff
abderrahmane <ilahalaha0987@gmail.com>
2024-01-13 11:35:21 +0100

    modifying the page

--- src/app/[lang]/(marketing)/page.tsx
+++ src/app/[lang]/(marketing)/page.tsx
@@ -18,7 +18,7 @@ export default async  function Home ({ params: { lang }} : LangParam){
     <div className='maxScreenWidth relative text-white w-full ' >    <FirstUnlogedHeader/></div>
 <AppHero/>
 <div className=" w-full flex items-center flex-col justify-center" >
-<div style={{maxWidth :"1500px"}} className="flex  items-center justify-center flex-col" >
+<div style={{maxWidth :"1500px"}} className="flex items-center justify-center flex-col" >
 
 
 
