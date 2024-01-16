51a05c0aa1f41d074e8dbaabf39b2bc38d6803f5
abderrahmane <ilahalaha0987@gmail.com>
2024-01-17 00:45:42 +0100

    integrating the compressed image in the footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -34,7 +34,7 @@ export const PrevFooter : React.FC  = async ()=>{
 
 export const Footer : React.FC = async ()=>{
     return <footer className="relative w-full h-fit text-white px-1 py-4 laptop:py-7 laptop:px-32 gap-8 flex flex-col items-center " >
-<img src="/footerBg.jpg" className="w-full h-full  absolute top-0 left-0" />
+<img src="/compresedCities/footerBg.jpg" className="w-full h-full  absolute top-0 left-0" />
 <div className="heroOverlay w-full h-full bg-opacity-40 absolute top-0 left-0" ></div>
 
     <div  className="flex relative z-30 items-center    justify-between w-full" >
